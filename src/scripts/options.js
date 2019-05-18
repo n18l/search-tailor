1c6e16133d473f261b9fe22817664795096f3ba9
Nick Brombal <nick.brombal@publicishawkeye.com>
2019-05-18 12:09:54 -0700

    [#6] Implement functional prototype of Tailoring Template list

--- src/scripts/options.js
+++ src/scripts/options.js
@@ -13,6 +13,13 @@ function isValidJson(json) {
     }
 }
 
+function generateID(maxRandomInt = 100000) {
+    const currentTimestamp = Date.now();
+    const randomInt = Math.floor(Math.random() * Math.floor(maxRandomInt));
+
+    return `${currentTimestamp}-${randomInt}`;
+}
+
 /* Class representing the extension options panel. */
 class TailoredSearchOptionsPanel {
     /**
@@ -170,9 +177,321 @@ class TailoredSearchOptionsPanel {
     }
 }
 
+
+const tailoringTemplateTemplate = document.querySelector("template#tailoring-template");
+
+/* Class representing an entry in the list of tailored domains. */
+class TailoringTemplate {
+    /**
+     * Initialize the list entry.
+     * @param {object} parentTailoringTemplateList - The list object to add this entry to.
+     * @param {string} [tailoringTemplateSettings] - An object containing the settings to use for this entry.
+     */
+    constructor(
+        parentTailoringTemplateList,
+        tailoringTemplateSettings = {
+            id: generateID(),
+            label: "",
+            backgroundColor: addonData.defaultUserData.tailoringTemplates[0].backgroundColor,
+            backgroundOpacity: addonData.defaultUserData.tailoringTemplates[0].backgroundOpacity,
+            borderColor: addonData.defaultUserData.tailoringTemplates[0].borderColor,
+            borderOpacity: addonData.defaultUserData.tailoringTemplates[0].borderOpacity,
+        }
+    ) {
+        this.parentList = parentTailoringTemplateList;
+        this.element = document
+            .importNode(tailoringTemplateTemplate.content, true)
+            .querySelector(".js-tailoring-template");
+        this.idInput = this.element.querySelector(".js-template-id-input");
+        this.labelInput = this.element.querySelector(".js-template-label-input");
+        this.backgroundColorInput = this.element.querySelector(
+            ".js-template-background-color-input"
+        );
+        this.backgroundOpacityInput = this.element.querySelector(
+            ".js-template-background-opacity-input"
+        );
+        this.borderColorInput = this.element.querySelector(
+            ".js-template-border-color-input"
+        );
+        this.borderOpacityInput = this.element.querySelector(
+            ".js-template-border-opacity-input"
+        );
+
+        this.defineActions();
+        this.bindEvents();
+
+        if (tailoringTemplateSettings.id) {
+            this.idInput.value = tailoringTemplateSettings.id;
+        }
+
+        if (tailoringTemplateSettings.label) {
+            this.labelInput.value = tailoringTemplateSettings.label;
+        }
+
+        if (tailoringTemplateSettings.backgroundColor) {
+            this.backgroundColorInput.value = tailoringTemplateSettings.backgroundColor;
+        }
+
+        if (tailoringTemplateSettings.backgroundOpacity) {
+            this.backgroundOpacityInput.value = tailoringTemplateSettings.backgroundOpacity;
+            this.backgroundColorInput.style.opacity = tailoringTemplateSettings.backgroundOpacity;
+        }
+
+        if (tailoringTemplateSettings.borderColor) {
+            this.borderColorInput.value = tailoringTemplateSettings.borderColor;
+        }
+
+        if (tailoringTemplateSettings.borderOpacity) {
+            this.borderOpacityInput.value = tailoringTemplateSettings.borderOpacity;
+            this.borderColorInput.style.opacity = tailoringTemplateSettings.borderOpacity;
+        }
+
+        this.parentList.element.appendChild(this.element);
+        this.labelInput.focus();
+    }
+
+    /**
+     * Get the value of the entry as an object.
+     */
+    get value() {
+        return {
+            id: this.idInput.value,
+            label: this.labelInput.value,
+            backgroundColor: this.backgroundColorInput.value,
+            backgroundOpacity: this.backgroundOpacityInput.value,
+            borderColor: this.borderColorInput.value,
+            borderOpacity: this.borderOpacityInput.value,
+        };
+    }
+
+    /**
+     * Identify and assign the entry's action buttons.
+     */
+    defineActions() {
+        this.actionButtons = {};
+        const actionButtons = this.element.querySelectorAll(
+            "[data-click-action]"
+        );
+
+        actionButtons.forEach(actionButton => {
+            this.actionButtons[actionButton.dataset.clickAction] = actionButton;
+        });
+    }
+
+    /**
+     * Attach event handlers.
+     */
+    bindEvents() {
+        this.labelInput.addEventListener("change", () =>
+            this.parentList.syncToStorage()
+        );
+
+        this.labelInput.addEventListener("input", () =>
+            this.parentList.validateEntries(
+                () => this.parentList.enableNewEntries(),
+                () => this.parentList.disableNewEntries()
+            )
+        );
+
+        this.labelInput.addEventListener("keypress", e => {
+            if (e.key === " ") e.preventDefault();
+        });
+
+        this.backgroundColorInput.addEventListener("change", () =>
+            this.parentList.syncToStorage()
+        );
+
+        this.backgroundOpacityInput.addEventListener("change", (e) => {
+            this.backgroundColorInput.style.opacity = e.target.value;
+            this.parentList.syncToStorage()
+        });
+
+        this.borderColorInput.addEventListener("change", () =>
+            this.parentList.syncToStorage()
+        );
+
+        this.borderOpacityInput.addEventListener("change", (e) => {
+            this.borderColorInput.style.opacity = e.target.value;
+            this.parentList.syncToStorage()
+        });
+
+        this.actionButtons.deleteTemplate.addEventListener("click", () =>
+            this.delete()
+        );
+    }
+
+    /**
+     * Reset the entry to its default state.
+     */
+    reset() {
+        const defaultTailoringTemplate = addonData.defaultUserData.tailoringTemplates[0];
+
+        this.idInput.value = defaultTailoringTemplate.id;
+        this.labelInput.value = defaultTailoringTemplate.label;
+        this.backgroundColorInput.value = defaultTailoringTemplate.backgroundColor;
+        this.backgroundOpacityInput.value = defaultTailoringTemplate.backgroundOpacity;
+        this.borderColorInput.value = defaultTailoringTemplate.borderColor;
+        this.borderOpacityInput.value = defaultTailoringTemplate.borderOpacity;
+    }
+
+    /**
+     * Remove the entry from its parent list, or reset it if it is the only
+     * remaining entry.
+     */
+    delete() {
+        const numberOfEntries = this.parentList.entries.length;
+
+        if (numberOfEntries === 1) {
+            this.reset();
+        } else {
+            const entryIndex = this.parentList.entries.indexOf(this);
+            this.parentList.entries.splice(entryIndex, 1);
+            this.element.remove();
+        }
+
+        this.parentList.validateEntries(
+            () => this.parentList.enableNewEntries(),
+            () => this.parentList.disableNewEntries()
+        );
+        this.parentList.syncToStorage();
+    }
+
+    /**
+     * Test the entry's validity.
+     * @returns {boolean} - Whether or not this entry is valid.
+     */
+    validate() {
+        const validityRequirements = [
+            this.idInput.value !== "",
+            this.labelInput.value !== "",
+            this.backgroundColorInput.value !== "",
+            this.borderColorInput.value !== "",
+        ];
+
+        return !validityRequirements.includes(false);
+    }
+}
+
+/** Class representing the interactive list of tailored domains. */
+class TailoringTemplateList {
+    /**
+     * Initialize the list UI.
+     * @param {object} entryListElement - The HTML element representing the list.
+     */
+    constructor(entryListElement) {
+        this.entries = [];
+        this.element = entryListElement;
+        this.addTemplateButton = document.querySelector(
+            '[data-click-action="addTemplate"]'
+        );
+
+        this.bindEvents();
+
+        browser.storage.sync
+            .get("tailoringTemplates")
+            .then(storageData => this.populate(storageData), logError);
+    }
+
+    /**
+     * Get all of the list's current entries.
+     * @returns {array} - An array of the current values of each list item.
+     */
+    get entryValues() {
+        return this.entries.map(entry => entry.value);
+    }
+
+    /**
+     * Attach event handlers.
+     */
+    bindEvents() {
+        document.addEventListener("click", e => {
+            const clickTarget = e.target.closest("[data-click-action]");
+
+            if (clickTarget && clickTarget.dataset.clickAction === "addTemplate") {
+                this.disableNewEntries();
+                this.entries.push(new TailoringTemplate(this));
+            }
+        });
+    }
+
+    /**
+     * Populate the list with existing extension data or an empty entry.
+     * @param {object} storageData - Existing extension data passed by the browser.storage API.
+     */
+    populate(storageData) {
+        if (
+            !storageData.tailoringTemplates ||
+            storageData.tailoringTemplates.length === 0
+        ) {
+            this.disableNewEntries();
+            this.entries.push(new TailoringTemplate(this));
+        }
+
+        storageData.tailoringTemplates.forEach(tailoringTemplateSettings =>
+            this.entries.push(
+                new TailoringTemplate(this, tailoringTemplateSettings)
+            )
+        );
+    }
+
+    /**
+     * Prevent the addition of new entries by users.
+     */
+    disableNewEntries() {
+        this.addTemplateButton.setAttribute("disabled", "");
+    }
+
+    /**
+     * Allow the addition of new entries by users.
+     */
+    enableNewEntries() {
+        this.addTemplateButton.removeAttribute("disabled");
+    }
+
+    /**
+     * Validate each of the list's entries.
+     * @param {function} [validCallback] - A function to call if all entries are valid.
+     * @param {function} [invalidCallback] - A function to call if any entries are invalid.
+     * @returns {array} - An array representing each entry's validity state.
+     */
+    validateEntries(validCallback = null, invalidCallback = null) {
+        const entryValidityStates = this.entries.map(entry => entry.validate());
+
+        if (invalidCallback && entryValidityStates.includes(false)) {
+            invalidCallback();
+        } else if (validCallback) {
+            validCallback();
+        }
+
+        return entryValidityStates;
+    }
+
+    /**
+     * Synchronize the list's current entries with the browser.storage API,
+     * then reinitialize the addon in any tabs affected by the extension.
+     */
+    syncToStorage() {
+        const validTailoringTemplates = this.entryValues.filter(
+            entryValueSet => entryValueSet.domain !== ""
+        );
+
+        browser.storage.sync
+            .set({ tailoringTemplates: validTailoringTemplates })
+            .then(null, logError);
+    }
+}
+
+
+
 const currentOptionsPanel = (function initOptionsPanel() {
     return new TailoredSearchOptionsPanel(
         document.querySelector("#search-tailor-options-panel")
+    );
+})();
+
+const currentTailoringTemplatesList = (function initTailoringTemplatesPanel() {
+    return new TailoringTemplateList(
+        document.querySelector("#search-tailor-tailoring-templates")
     );
 })();
 
