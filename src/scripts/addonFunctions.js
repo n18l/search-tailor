086a45a9876e44ba8884b91635453538a6b19017
Nick Brombal <nick.brombal@publicishawkeye.com>
2020-03-07 18:20:30 -0800

    Add function for converting hex colour format to HSL

--- src/scripts/addonFunctions.js
+++ src/scripts/addonFunctions.js
@@ -40,4 +40,61 @@ const addonFunctions = {
             element.title = tailoringTemplate.label;
         }
     },
+
+    /**
+     * Converts a given hex color string value into HSL (Hue, Saturation,
+     * Lightness) values, either as an array or a CSS-compatible string.
+     * @param {string} hexString - The hex value to be converted.
+     * @param {boolean} returnAsString - Whether or not to return the converted value as a CSS-compatible string. Defaults to false.
+     * @param {integer} [alphaValue] - An optional alpha value to apply when outputting as a string, between 0 and 1 inclusive. Defaults to 1.
+     */
+    hexToHSL(hexString, returnAsString = false, alphaValue = 1) {
+        if (!hexString) {
+            return console.error(
+                "hexToHSL function requires a valid hex string."
+            );
+        }
+
+        const r = parseInt(hexString.substr(1, 2), 16) / 255;
+        const g = parseInt(hexString.substr(3, 2), 16) / 255;
+        const b = parseInt(hexString.substr(5, 2), 16) / 255;
+        const max = Math.max(r, g, b);
+        const min = Math.min(r, g, b);
+
+        let hue;
+        let saturation;
+        const lightness = (max + min) / 2;
+
+        if (max === min) {
+            hue = 0;
+            saturation = 0;
+        } else {
+            const delta = max - min;
+
+            switch (max) {
+                case r:
+                    hue = (g - b) / delta + (g < b ? 6 : 0);
+                    break;
+                case g:
+                    hue = (b - r) / delta + 2;
+                    break;
+                case b:
+                    hue = (r - g) / delta + 4;
+                    break;
+                default:
+                    break;
+            }
+
+            hue = Math.round(hue / (6 / 360));
+            saturation =
+                lightness > 0.5 ? delta / (2 - max - min) : delta / (max + min);
+        }
+
+        if (returnAsString) {
+            return `hsla(${hue}, ${saturation * 100}%, ${lightness *
+                100}%, ${alphaValue})`;
+        }
+
+        return [hue, saturation, lightness];
+    },
 };
