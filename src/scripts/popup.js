0e7fed7886771bbb2824c145a41e6321bde38218
Nick Brombal <nick.brombal@publicishawkeye.com>
2020-03-21 23:58:48 -0700

    Namespace popup-related methods

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -4,36 +4,41 @@ const TailoringGroup = require("./TailoringGroup");
 const TailoringEntry = require("./TailoringEntry");
 
 /**
- * Creates a Tailoring Group for each existing treatment in the user data,
- * adding it to the current working list of all Tailoring Groups.
+ * Namespace for popup-related properties & methods.
  */
-function initializeGroups() {
-    addonData.local.tailoringTreatments.forEach(tailoringTreatment => {
-        addonData.local.tailoringGroups.push(
-            new TailoringGroup(tailoringTreatment)
-        );
-    });
-}
+const popup = {
+    /**
+     * Creates a Tailoring Group for each existing treatment in the user data,
+     * adding it to the current working list of all Tailoring Groups.
+     */
+    initializeGroups() {
+        addonData.local.tailoringTreatments.forEach(tailoringTreatment => {
+            addonData.local.tailoringGroups.push(
+                new TailoringGroup(tailoringTreatment)
+            );
+        });
+    },
 
-/**
- * Creates a Tailoring Entry for each existing entry in the user data, adding it
- * to the entry list of the Tailoring Group to which it belongs.
- */
-function initializeEntries() {
-    addonData.local.tailoredDomains.forEach(tailoredDomain => {
-        addonFunctions
-            .getTailoringGroupByTreatmentID(tailoredDomain.treatment)
-            .entries.push(new TailoringEntry(tailoredDomain));
-    });
-}
+    /**
+     * Creates a Tailoring Entry for each existing entry in the user data,
+     * adding it to the entry list of the Tailoring Group to which it belongs.
+     */
+    initializeEntries() {
+        addonData.local.tailoredDomains.forEach(tailoredDomain => {
+            addonFunctions
+                .getTailoringGroupByTreatmentID(tailoredDomain.treatment)
+                .entries.push(new TailoringEntry(tailoredDomain));
+        });
+    },
 
-/**
- * Initializes the addon's popup UI.
- */
-function initializePopup() {
-    initializeGroups();
-    initializeEntries();
-}
+    /**
+     * Initializes the addon's popup UI.
+     */
+    initialize() {
+        this.initializeGroups();
+        this.initializeEntries();
+    },
+};
 
 // Get the current user data, then initialize the popup UI.
-addonFunctions.getUserData().then(() => initializePopup());
+addonFunctions.getUserData().then(() => popup.initialize());
