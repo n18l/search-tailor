43e8ee57053bdd2ba0b6c1f40fd66507e9b435a9
Nick Brombal <nick.brombal@publicishawkeye.com>
2020-04-17 17:51:35 -0700

    Prevent weird placeholders when dragging in Chrome
    Chrome extensions appear to have issues with SortableJS'
    dragging when using the HTML5 drag & drop API. Force the
    library to use its own fallback methods to avoid the issue.

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -84,6 +84,7 @@ const popup = {
         return new Sortable(document.querySelector(".js-entry-container"), {
             handle: ".js-sort-handle",
             animation: 150,
+            forceFallback: true,
             onUpdate(event) {
                 // Remove the entry from it's old position in the object array,
                 // capturing a copy of it as we do so.
